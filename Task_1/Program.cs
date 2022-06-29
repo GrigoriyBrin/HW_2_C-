d8fc1156604693371b5ea447a9874ab4902e10c6
GrigoriyBrin <brinzeigrigorii@gmail.com>
2022-06-29 10:37:52 +0300

    Решение задачи 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3-х значное число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string b = a.ToString();
+            int countSymbol = 4;
+            
+            if (b.Length < countSymbol)
+            {
+                Console.WriteLine("Result: " + b[1]);
+            }
+        }
+    }
+}
